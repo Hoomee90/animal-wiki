faa0d5d290ab5cfa4bfe5b53dc08cbc8dce17b95
Samantha Callie <Hoomee90@gmail.com>
2023-08-10 15:02:34 -0700

    add if else statements to hide other form results on resubmission

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 // UI logic
 
+function formEventShow(animal) {
+  document.querySelector("div#" + animal).removeAttribute("class");
+}
+
+function formEventHide(...Allanimal) {
+  Allanimal.forEach((animal) => document.querySelector("div#" + animal).setAttribute("class", "hidden"));
+}
+
 function formSubmissionEventHandler() {
   let form = document.querySelector("#form");
   form.onsubmit = function (event) {
     event.preventDefault();
     let animalInput = document.getElementById("animalInput").value;
-    if (animalInput === "notsnake") {
-      console.log(animalInput);
-    } else if (animalInput === "ontovore") {
-      console.log(animalInput);
-    } else if (animalInput === "dog") {
-      console.log(animalInput);
+    if (animalInput === "notsnake"||animalInput === "ontovore"||animalInput === "dog") {
+      formEventShow(animalInput);
+      if (animalInput === "notsnake") {
+        formEventHide("ontovore", "dog");
+      } else if (animalInput === "ontovore") {
+        formEventHide("notsnake", "dog");
+      } else if (animalInput === "dog") {
+        formEventHide("notsnake", "ontovore");
+      }
     }
   }
 }
