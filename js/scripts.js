d2f535af9ee1b239c74b055fdcbec23d944079f6
Samantha Callie <Hoomee90@gmail.com>
2023-08-11 13:43:27 -0700

    fixed bug with the function for hiding form results

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function formEventShow(animal) {
 }
 
 function formEventHide(...Allanimal) {
-  Allanimal.forEach((animal) => document.querySelector("div#" + animal).setAttribute("class", "hidden"));
+  Allanimal.forEach((animal) => document.querySelector("div#" + animal).classList.add("hidden"));
 }
 
 function formSubmissionEventHandler() {
@@ -15,20 +15,22 @@ function formSubmissionEventHandler() {
     
     let animalInput = document.getElementById("animalInput").value;
     const button = document.querySelector("button");
-    const animalEntries = Array.from(document.querySelectorAll("div.infocard"));
+    const animalEntries = Array.from(document.querySelectorAll(".infocard"));
     const animalTypes = animalEntries.map(element => element.id);
 
     if (animalTypes.includes(animalInput)) {
       button.innerText = "Enter";
-      
       document.getElementById("animalInput").value = "";
+      
       formEventShow(animalInput);
       
       if (animalInput === "notsnake") {
         formEventHide("ontovore", "dog");
-      } else if (animalInput === "ontovore") {
+      } 
+      else if (animalInput === "ontovore") {
         formEventHide("notsnake", "dog");
-      } else if (animalInput === "dog") {
+      } 
+      else if (animalInput === "dog") {
         formEventHide("notsnake", "ontovore");
       }
     } 
